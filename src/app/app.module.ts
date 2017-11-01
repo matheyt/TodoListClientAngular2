83bd726200f92bcbf39df8f5f28ef4dd0b71b493
nathan07 <nathan.prinsac@iut-valence.fr>
2017-11-01 18:51:13 +0100

    correction merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
 
 import {MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule, MatSelectModule,
-  MatOptionModule, MatDialogModule} from '@angular/material';
+  MatOptionModule, MatDialogModule, MatExpansionModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -35,7 +35,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule, HttpModule, FormsModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule,
-    BrowserAnimationsModule, MatSelectModule, MatOptionModule, NoopAnimationsModule, MatDialogModule,
+    BrowserAnimationsModule, MatSelectModule, MatOptionModule, NoopAnimationsModule, MatDialogModule, MatExpansionModule,
     RouterModule.forRoot(appRoutes, {useHash: true} )
   ],
   providers: [TodoListService],
